47712ab37adf232726438a0d2d145b06118d052b
TihonovVlad <robobox2021@mail.ru>
2026-09-07 00:13:02 +0300

    Start

---
+++ src/main/java/com/swapgroup/bomber/BomberApplication.java
@@ -0,0 +1,13 @@
+package com.swapgroup.bomber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BomberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swapgroup/bomber/BomberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swapgroup.bomber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
